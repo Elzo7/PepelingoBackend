ec4b3e4292400f6c1f7b4bb0f089e6ce06924126
Damian Traczuk <damiantra7@gmail.com>
2023-12-01 16:05:17 +0100

    Test Cors v2

--- src/main/java/com/example/pepelingbackendv2/configs/SecurityConfig.java
+++ src/main/java/com/example/pepelingbackendv2/configs/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers("/auth/signup","/auth/login").permitAll()
